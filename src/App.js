b67b51c9037d04d1ebb842b105d09661361ee4fd
Ghost General <awotundun@gmail.com>
2022-02-05 13:59:15 +0100

    event props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+
+
 import FeedbackData from "./data/FeedbackData"; 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -14,6 +17,7 @@ function App() {
         <>
         <Header />
         <div className="container">
+            <FeedbackStats feedback={feedback} /> 
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/> 
            
         </div>
